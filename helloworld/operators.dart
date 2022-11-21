fa51ccab26b302f598caee7370cdf05a18b27a67
karlos <cargb2003@gmail.com>
2022-11-21 16:55:57 -0500

    Adding operators folder

---
+++ helloworld/operators.dart
@@ -0,0 +1,9 @@
+void main() {
+  int suma = 5 + 1 + 8;
+  double substraction = 6 - 7 + 8;
+  int multiplication = 4 * 2;
+  double division = 15 / 5;
+  double mod = 5 % 3;
+  print(mod);
+  print(suma);
+}
